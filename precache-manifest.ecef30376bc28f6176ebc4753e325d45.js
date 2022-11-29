4edecf161eff3bd196fbe5aa922af2de80ad0f39
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-11-29 12:14:56 +0100

    Updates

---
+++ precache-manifest.ecef30376bc28f6176ebc4753e325d45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdf54fd76e2ed279626720d2318a7c7",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ce61a575ee83ab5fbe0e",
+    "url": "/todolist/static/css/main.105b304b.chunk.css"
+  },
+  {
+    "revision": "afe28ef659bc0abb9649",
+    "url": "/todolist/static/js/2.c72d94a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.c72d94a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce61a575ee83ab5fbe0e",
+    "url": "/todolist/static/js/main.8036659c.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
