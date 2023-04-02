3b54218b695a36d9cfa10399d8351f8127bd8350
Anastasiya Rabetskaya <nastia1362@gmail.com>
2023-04-02 12:37:16 +0300

    add removeTask to tasks-reducer

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,21 +1,29 @@
 import {TasksStateType} from "../App";
 
+type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+type ActionsType = RemoveTaskACType
 
-export const tasksReducer = (state: TasksStateType, action: any) => {
+export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
     switch (action.type) {
-        case '':
-            return state
-        case '' :
-            return state
+        case 'REMOVE-TASK':
+            return {...state, [action.todolistId]: state[action.todolistId].filter(t => t.id !== action.taskId)}
         default:
             throw new Error('I don\'t understand this type')
     }
 }
 
-export const firstAC = () => {
-    return {}
+export const removeTaskAC = (taskId: string, todolistId: string) => {
+    return {
+        type: 'REMOVE-TASK',
+        taskId,
+        todolistId
+    } as const
 }
 export const secondAC = () => {
     return {}
 }
+export const thirdAC = () => {
+    return {}
+}
+
 
